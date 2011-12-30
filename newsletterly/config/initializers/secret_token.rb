622c272b4e2dcd8051368253ba82d34f08712fc7
Jason White <jasonwhitestl@me.com>
2011-12-30 13:56:24 -0600

    first commit

---
+++ newsletterly/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsletterly::Application.config.secret_token = '1cad603630344a90e24837fac1dbec0aed785144b354a5e11e0efcdf2028788a333e2790efa311a6861f1854c461208eeba11509ad9f1a67d770ab398398e9ee'
